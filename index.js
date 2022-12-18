9eef2fc7ff154b585b349d57cfcf19cc24c3ea61
Charbel Maroun <charbel-maroun@hotmail.com>
2022-12-18 16:31:02 -0500

    create populateCard function to create card when a name is searched or leaderboard name selected

--- index.js
+++ index.js
@@ -47,3 +47,21 @@ function leaderboardPlayer(data) {
 
     }
 }
+
+function populateCard(data) {
+    document.querySelectorAll('.place-card').forEach(element => element.style.display = 'none')
+    document.querySelector('#pop-up-card').style.display = 'block'
+
+    document.querySelector('.pop-up-img').src = data.avatar
+    document.querySelector('.card-title-pop-up').textContent = data.username
+    document.querySelector('.pop-up-group').children[0].textContent = `Player ID: ${data.player_id}`
+    document.querySelector('.pop-up-group').children[1].textContent = `Country: ${regionNames.of(data.country.slice(-2))}`
+
+    document.querySelector('.pop-up-group').children[2].textContent = `Status: ${data.status}`
+    document.querySelector('.pop-up-profile').href = `https://www.chess.com/member/${data.username}`
+
+    document.querySelector('.leaderboard-list').style.height = '45rem'
+    document.querySelector('.pop-up-follow').style.backgroundColor = ""
+    document.querySelector('.pop-up-follow').innerText = "Follow!"
+
+}
